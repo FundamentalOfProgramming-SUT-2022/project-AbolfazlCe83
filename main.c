96a544776e7463c18eff8c960454457a81d7d224
AbolfazlCe83 <119687358+AbolfazlCe83@users.noreply.github.com>
2023-02-04 16:11:29 +0330

    Copy command updated

--- main.c
+++ main.c
@@ -7,8 +7,12 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <windows.h>
 #define MAX_SIZE 1000
+#define FILENAME_SIZE 1024
+#define MAX_LINE 2048
 
+//https://www.tutorialkart.com/c-programming/c-read-text-file/(for Cat function)
 //https://codeforwin.org/c-programming/c-program-to-list-all-files-in-a-directory-recursively(for tree function)
 
 char command[MAX_SIZE];
@@ -18,7 +22,7 @@ int error_flag = 0;
 void createfolder(const char directory[]);
 void createfile(char directory[]);
 void cat(FILE *fp , char fileaddress[]);
-void copy(FILE *fp , int line_number , int char_pos , char b_f[] , char fille_addres[]);
+void copy(FILE *fp , int line_number , int char_pos ,int number_of_chars , char b_f[] , char fille_addres[]);
 
 
 int main() {
@@ -113,104 +117,111 @@ int main() {
                 printf("invalid command\nfor more information type <help>!\n");
             }
         }
-        // else if(!(strcmp(command , "copy")))
-        // {
-        //     FILE *for_function;
-        //     char FILLE_ADDRESS[MAX_SIZE];
-        //     char buffer[50];
-        //     getchar();
-        //     scanf("%s" , buffer);
-        //     if(!(strcmp(buffer , "--file")))
-        //     {
-        //         char file_address[200];
-        //         char directory[200];
-        //         getchar();
-        //         scanf("%s" , file_address);
-        //         int address_length = strlen(file_address);
-        //         if(file_address[0] != '/' && file_address[0] != '"') {
-        //             char chert[MAX_SIZE];
-        //             scanf("%[^\n]s" , chert);
-        //             printf("invalid command\nfor more information type <help>!\n");
-        //         }
-        //         else if(file_address[0] == '"' && file_address[address_length - 1] == '"') {
-        //             for(int i = 0; i < address_length - 2; i++) {
-        //             directory[i] = file_address[i + 1];
-        //             strcpy(FILLE_ADDRESS , directory + 1);
-        //             }
-        //             FILE *for_read = fopen(directory + 1 , "r");
-        //             fclose(for_read);
-        //             if(!for_read) {
-        //                 printf("The file doesnt exist!\n");
-        //             }
-        //             else
-        //             {
-        //                 for_function = for_read;
-        //             }
-        //         }
-        //         else 
-        //         {
-        //             FILE *forread = fopen(file_address + 1 , "r");
-        //             fclose(forread);
-        //             if(!forread) {
-        //                 printf("The file doesnt exist!\n");
-        //             }
-        //             else
-        //             {
-        //                 for_function = forread;
-        //                 strcpy(FILLE_ADDRESS , file_address + 1);
-        //             }
+        else if(!(strcmp(command , "copy")))
+        {
+            FILE *for_function;
+            char FILLE_ADDRESS[MAX_SIZE];
+            char buffer[50];
+            getchar();
+            scanf("%s" , buffer);
+            if(!(strcmp(buffer , "--file")))
+            {
+                char file_address[200];
+                char directory[200];
+                getchar();
+                scanf("%s" , file_address);
+                int address_length = strlen(file_address);
+                if(file_address[0] != '/' && file_address[0] != '"') {
+                    char chert[MAX_SIZE];
+                    scanf("%[^\n]s" , chert);
+                    printf("invalid command\nfor more information type <help>!\n");
+                }
+                else if(file_address[0] == '"' && file_address[address_length - 1] == '"') {
+                    for(int i = 0; i < address_length - 2; i++) {
+                    directory[i] = file_address[i + 1];
+                    strcpy(FILLE_ADDRESS , directory + 1);
+                    }
+                    FILE *for_read = fopen(directory + 1 , "r");
+                    fclose(for_read);
+                    if(!for_read) {
+                        printf("The file doesnt exist!\n");
+                    }
+                    else
+                    {
+                        for_function = for_read;
+                    }
+                }
+                else 
+                {
+                    FILE *forread = fopen(file_address + 1 , "r");
+                    fclose(forread);
+                    if(!forread) {
+                        printf("The file doesnt exist!\n");
+                    }
+                    else
+                    {
+                        for_function = forread;
+                        strcpy(FILLE_ADDRESS , file_address + 1);
+                    }
                     
-        //         }
+                }
                 
-        //         char position[50];
-        //         getchar();
-        //         scanf("%s" , position);
-        //         if(!(strcmp(position , "--pos")))
-        //         {
-        //             int line_number = 0;
-        //             char Two_dat;
-        //             int char_pos = 0;
-        //             scanf("%d%c%d" , &line_number , &Two_dat , &char_pos);
-        //             char size[50];
-        //             getchar();
-        //             scanf("%s" , size);
-        //             if(!(strcmp(size , "-size")))
-        //             {
-        //                 int number_of_chars = 0;
-        //                 scanf("%d" , number_of_chars);
-        //                 getchar();
-        //                 char back_or_forward[3];
-        //                 gets(back_or_forward);
-        //                 if(!(strcmp(back_or_forward , "-b") || strcmp(back_or_forward , "-f")))
-        //                     copy(for_function , line_number , char_pos , back_or_forward , FILLE_ADDRESS);
-        //                 else
-        //                 {
-        //                     char chert[MAX_SIZE];
-        //                     scanf("%[^\n]s" , chert);
-        //                     printf("invalid command\nfor more information type <help>!\n");
-        //                 }
-        //             }
-        //             else
-        //             {
-        //                 char chert[MAX_SIZE];
-        //                 scanf("%[^\n]s" , chert);
-        //                 printf("invalid command\nfor more information type <help>!\n");
-        //             }
-        //         }
-        //         else
-        //         {
-        //             char chert[MAX_SIZE];
-        //             scanf("%[^\n]s" , chert);
-        //             printf("invalid command\nfor more information type <help>!\n");
-        //         }
-        //     }
-        //     else
-        //     {
-        //         char chert[MAX_SIZE];
-        //         scanf("%[^\n]s" , chert);
-        //         printf("invalid command\nfor more information type <help>!\n");
-        //     }
-        // }
+                char position[50];
+                getchar();
+                scanf("%s" , position);
+                if(!(strcmp(position , "--pos")))
+                {
+                    int line_number = 0;
+                    char Two_dat;
+                    int char_pos = 0;
+                    scanf("%d%c%d" , &line_number , &Two_dat , &char_pos);
+                    char size[50];
+                    getchar();
+                    scanf("%s" , size);
+                    if(!(strcmp(size , "-size")))
+                    {
+                        int number_of_chars = 0;
+                        scanf("%d" , &number_of_chars);
+                        getchar();
+                        char back_or_forward[3];
+                        gets(back_or_forward);
+                        
+                        if(!(strcmp(back_or_forward , "-b")))
+                        {
+                            copy(for_function , line_number , char_pos , number_of_chars , back_or_forward , FILLE_ADDRESS);
+                        }
+                        else if (!(strcmp(back_or_forward , "-f")))
+                        {
+                            copy(for_function , line_number , char_pos , number_of_chars ,back_or_forward , FILLE_ADDRESS);
+                        }
+                        else
+                        {
+                            char chert[MAX_SIZE];
+                            scanf("%[^\n]s" , chert);
+                            printf("invalid command\nfor more information type <help>!\n");
+                        }
+                    }
+                    else
+                    {
+                        char chert[MAX_SIZE];
+                        scanf("%[^\n]s" , chert);
+                        printf("invalid command\nfor more information type <help>!\n");
+                    }
+                }
+                else
+                {
+                    char chert[MAX_SIZE];
+                    scanf("%[^\n]s" , chert);
+                    printf("invalid command\nfor more information type <help>!\n");
+                }
+            }
+            else
+            {
+                char chert[MAX_SIZE];
+                scanf("%[^\n]s" , chert);
+                printf("invalid command\nfor more information type <help>!\n");
+            }
+        }
         else
         {
             char chert[MAX_SIZE];
@@ -252,41 +263,7 @@ void createfolder(const char directory[]){
             break;
         index_of_end = 0;
     }
-    
-    // int directory_length = strlen(directory);
-    // int index_end = 0;
-    // int slash_counter = 0;
-    // char newdir[MAX_SIZE];
-    // for(int i = 0; i < directory_length; i++) {
-    //     if(directory[i] == '/') {
-    //         slash_counter++;
-    //     }
-    // }
-    // char diiiiiir[MAX_SIZE];
-    // int first_slash = 0;
-    // int second_slash = 1;
-    // for(int j = 0 ; j < slash_counter ; j++) {
-    //     for(int i = 0 ; i < directory_length ; i++) {
-    //         if(directory[i] == '/') {
-    //             second_slash++;
-    //             if(second_slash > first_slash) {
-    //                 index_end = i;
-    //                 break;
-    //             }
-    //         }
-    //         if(index_end != 0) {
-    //         for(int k = 0; k < index_end; k++) {
-    //             newdir[k] = directory[k];
-    //         }
-    //         }
-    //     }
-    //     first_slash = second_slash;
-    //     second_slash = 0;
-    //     printf("%s\n" , newdir);
-    //     for(int i = 0; i < strlen(newdir) ; i++) {
-    //         newdir[i] = 0;
-    //     }
-    // }
+   
     }
 
 void createfile(char directory[]) {
@@ -317,15 +294,72 @@ void cat(FILE *fp , char fileaddress[]) {
     printf("\n");
 }
 
-// void copy(FILE *fp , int line_number , int char_pos , char b_f[] , char fileaddress[])
-// {
-//     fp = fopen(fileaddress , "r");
-//     char *content_of_file;
-//     long numbytes;
-//     fseek(fp , 0L , SEEK_END);
-//     numbytes = ftell(fp);
-//     fseek(fp , 0L , SEEK_SET);
-//     content_of_file = (char *)calloc(numbytes, sizeof(char));
-//     fread(content_of_file , sizeof(char) , numbytes , fp);
-//     fclose(fp);
-// }
+void copy(FILE *fp , int line_number , int char_pos , int number_of_chars,char b_f[] , char fileaddress[])
+{
+    fp = fopen(fileaddress , "r");
+    char filename[FILENAME_SIZE];
+    char buffer[MAX_LINE];
+    bool keep_reading = true;
+    int reade_line = line_number;
+    int current_line = 1;
+    char copied[MAX_SIZE];
+    do
+    {
+        fgets(buffer , MAX_LINE , fp);
+        if(feof(fp))
+        {
+            keep_reading = false;
+            printf("This Line doesnt exist in file!\n");
+            printf("Please insert correct line!\n");
+
+        }
+        else if(current_line == reade_line) 
+        {
+            keep_reading = false;
+            if(strlen(buffer) - 1 <= char_pos)
+            {
+                printf("This charachter doesn't exist in file!\nthe number of charachters is : %d\n" , strlen(buffer) - 1);
+                break;
+            }
+            else if(!(strcmp(b_f , "-b")))
+            {
+                if((char_pos - number_of_chars + 1) < 0){
+                    printf("The size you specified does not exist in the file!\n");
+                    break;
+                }
+                else
+                {
+                for(int i = char_pos - number_of_chars + 1; i <= char_pos; i++) {
+                    copied[i - char_pos + number_of_chars - 1] = buffer[i] ;
+                }
+                }
+            }
+            else if(!(strcmp(b_f , "-f")))
+            {
+                if((char_pos + number_of_chars - 1) >= strlen(buffer))
+                {
+                    printf("The size you specified does not exist in the file!\n");
+                    break;
+                }
+                else
+                {
+                for(int i = char_pos; i <= char_pos + number_of_chars - 1; i++) {
+                    copied[i - char_pos] = buffer[i];
+                }
+                }
+            }
+        }
+        current_line++;
+    } while (keep_reading);
+    fclose(fp);
+    FILE *clipboard;
+    clipboard = fopen("clipboard.txt" , "w");
+    fputs(copied , clipboard);
+    fclose(clipboard);
+    wchar_t *fileLPCWSTR = "clipboard.txt";
+    int attr = GetFileAttributes(fileLPCWSTR);
+    if((attr && FILE_ATTRIBUTE_HIDDEN) == 0) 
+    {
+        SetFileAttributes(fileLPCWSTR , attr | FILE_ATTRIBUTE_HIDDEN);
+    }
+}
